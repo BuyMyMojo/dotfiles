30e9041ac04a1eedbd99b35b231b3ab508d61819
BuyMyMojo <hello+git@buymymojo.net>
2025-03-31 09:57:01 +1100

    add Typr for neovim (typing speed test)

--- nvim/lua/plugins/main.lua
+++ nvim/lua/plugins/main.lua
@@ -1,4 +1,10 @@
 return {
 	{ "folke/todo-comments.nvim", opts = {} },
 	{ 'wakatime/vim-wakatime', lazy = false },
+	{
+    		"nvzone/typr",
+    		dependencies = "nvzone/volt",
+		opts = {},
+    		cmd = { "Typr", "TyprStats" },
+	},
 }
